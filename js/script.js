c56aa09fee5a2320969bccf666ef66d51fe96ad1
S-3KA <84847251+S-3KA@users.noreply.github.com>
2021-11-11 20:13:12 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
-document.getElementById("footer_banner").src = "";
-console.log(document.getElementById("footer_banner"));
+document.querySelector('#elastic').oninput = function(){
+    let val = this.value.trim();
+    let elasticItame = document.querySelectorAll('.elastic a');
+    if (val != ''){
+        elasticItame.forEach(function (elem){
+            if (elem.innerText.search(val) == -1){
+                elem.classList.add('hide');
+            }
+            else{
+                elem.classList.remove('hide');
+            }
+        });
+    }
+    else{
+        elasticItame.forEach(function(elem){
+            elem.classList.remove('hide');
+        })
+    }
+}
